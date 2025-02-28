e59b745d4c73d4bb012156f8c7e82198794d722b
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2025-02-28 13:09:15 +0000

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -8,38 +8,38 @@ echo "$region"
 cp header.meta meta_CADDALL.txt
 cp header.meta meta_CADD15.txt
 cp header.meta meta_CADD20.txt
-
+echo "Hedaer copied!"
 zgrep -v '#' f5.vcf.gz | cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
-
+echo "c6 just created!"
 
 ##merge;
 paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
 paste c1 c2 c3 c4 c5a c6 >> meta_CADD15.txt
 paste c1 c2 c3 c4 c5b c6 >> meta_CADD20.txt
+echo "C1 to C6 copied!"
 
 
 
-
-##CADD_all
-while read gene;
-    do
-        cp header.meta "metafilesALL_${region}/${gene}_CADDALL.meta"
-        grep -a -w "$gene" meta_CADDALL.txt >> "metafilesALL_${region}/${gene}_CADDALL.meta"
-        ###bgzip ${gene}_CADDALL.meta
-    done < gene.lst
+##CADD_all#
+#while read gene;
+#    do
+#        cp header.meta "metafilesALL_${region}/${gene}_CADDALL.meta"
+#        grep -a -w "$gene" meta_CADDALL.txt >> "metafilesALL_${region}/${gene}_CADDALL.meta"
+#        ###bgzip ${gene}_CADDALL.meta
+#    done < gene.lst
 
 ##CADD_15
-while read gene;
-    do
-        cp header.meta "metafiles15_${region}/${gene}_CADD15.meta"
-        grep -a -w "$gene" meta_CADD15.txt >> "metafiles15_${region}/${gene}_CADD15.meta"
-        ###bgzip ${gene}_CADD15.meta
-    done < gene.lst
+#while read gene;
+#    do
+#        cp header.meta "metafiles15_${region}/${gene}_CADD15.meta"
+#        grep -a -w "$gene" meta_CADD15.txt >> "metafiles15_${region}/${gene}_CADD15.meta"
+#        ###bgzip ${gene}_CADD15.meta
+#    done < gene.lst
 
 ##CADD_20
-while read gene;
-    do
-        cp header.meta "metafiles20_${region}/${gene}_CADD20.meta"
-        grep -a -w "$gene" meta_CADD20.txt >> "metafiles20_${region}/${gene}_CADD20.meta"
-        ###bgzip ${gene}_CADD20.meta
-    done < gene.lst
+#while read gene;
+#    do
+#        cp header.meta "metafiles20_${region}/${gene}_CADD20.meta"
+#        grep -a -w "$gene" meta_CADD20.txt >> "metafiles20_${region}/${gene}_CADD20.meta"
+#        ###bgzip ${gene}_CADD20.meta
+#    done < gene.lst
