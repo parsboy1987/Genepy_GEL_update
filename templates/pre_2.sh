8e8a5ff9860b8f0e99f845ad24b42c3475af63a6
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2025-03-03 13:27:23 +0000

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -1,9 +1,9 @@
-#zcat f5.vcf.gz | awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' > c6
-#awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' f5.vcf > c6
-> meta_CADDALL.txt
-> meta_CADD15.txt
-> meta_CADD20.txt
-##region=$1
+###zcat f5.vcf.gz | awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' > c6
+###awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' f5.vcf > c6
+###> meta_CADDALL.txt
+###> meta_CADD15.txt
+###> meta_CADD20.txt
+###region=$1
 echo "$region"
 cp header_meta meta_CADDALL.txt
 cp header_meta meta_CADD15.txt
@@ -13,10 +13,10 @@ zgrep -v '#' f5.vcf.gz | cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i
 echo "c6 just created!"
 
 ##merge;
-#paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
-#paste c1 c2 c3 c4 c5a c6 >> meta_CADD15.txt
-#paste c1 c2 c3 c4 c5b c6 >> meta_CADD20.txt
-#echo "C1 to C6 copied!"
+###paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
+###paste c1 c2 c3 c4 c5a c6 >> meta_CADD15.txt
+###paste c1 c2 c3 c4 c5b c6 >> meta_CADD20.txt
+###echo "C1 to C6 copied!"
 
 
 
