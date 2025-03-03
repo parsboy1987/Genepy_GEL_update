26388f9ed99774d0972cbd757fcc04a19dedad3c
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2025-03-03 16:27:11 +0000

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -4,27 +4,10 @@
 > meta_CADD15.txt
 > meta_CADD20.txt
 region=$1
-#c1=$1 
-#c2=$2 
-#c3=$3
-#c4=$4
-#c5=$5
-#c5a=$6 
-#c5b=$7 
-#gene.lst=$8 
-#f5.vcf.gz=$9 
-#header_meta=$10
-#vcf_n=$11 
-#chrx=$12 
-#region=$13
-##echo "this is ${region}"
-cat gene.lst | head
 cp header_meta meta_CADDALL.txt
 cp header_meta meta_CADD15.txt
 cp header_meta meta_CADD20.txt
-echo "Header copied!"
-zcat f5.vcf.gz | grep -v "#" | head | cut -f 1-10
-#zcat f5.vcf.gz | grep -v '#'| cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
+zcat f5.vcf.gz | grep -v "#"| cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
 echo "c6 just created!"
 
 ##merge;
