016f951424d94fe91fbb71d2e999794ddf171108
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2025-03-03 13:31:51 +0000

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -9,7 +9,7 @@ cp header_meta meta_CADDALL.txt
 cp header_meta meta_CADD15.txt
 cp header_meta meta_CADD20.txt
 echo "Hedaer copied!"
-zgrep -v '#' f5.vcf.gz | cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
+zcat f5.vcf.gz | grep -v '#'| cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
 echo "c6 just created!"
 
 ##merge;
