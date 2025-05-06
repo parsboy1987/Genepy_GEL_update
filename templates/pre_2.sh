7556312900d092fbc76a800ff4cb50a48847bee3
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2025-05-06 16:51:50 +0100

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -19,7 +19,9 @@ cp header_meta meta_CADD15.txt
 cp header_meta meta_CADD20.txt
 
 #zgrep -v "#" f5.vcf.gz | perl -F'\t' -lane 'print join("\t", map { substr($_,0,3) } @F[9..$#F])' > c6
-zgrep -v '#' f5.vcf.gz | cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
+#zgrep -v '#' f5.vcf.gz | cut -f 10- | awk -F"\t" '{OFS=FS}{for(i=1;i<=NF;i++) $i=substr($i,1,3)}1' >c6
+zgrep -v '#' f5.vcf.gz | cut -f10- | perl -F'\t' -lane 'print join("\t", map { substr($_,0,3) } @F)' > c6
+
 ##merge;
 paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
 paste c1 c2 c3 c4 c5a c6 >> meta_CADD15.txt
